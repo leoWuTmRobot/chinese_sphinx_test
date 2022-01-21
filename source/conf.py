f757693a71080053fa223f07d6701ab6a0e2a746
leo.wu <leo.wu@tm-robot.com>
2022-01-21 09:51:33 +0800

    try to use chinese latex

--- source/conf.py
+++ source/conf.py
@@ -60,6 +60,15 @@ exclude_patterns = []
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+
+latex_elements = {
+  # Additional stuff for the LaTeX preamble.
+    'preamble': r'''
+    \hypersetup{unicode=true}
+    \usepackage{CJKutf8}
+    \setCJKfamilyfont{kai}
+    ''',
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
